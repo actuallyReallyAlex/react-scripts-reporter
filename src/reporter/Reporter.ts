a419ec758b7c41d8b3dea4d3c9e2228e70eb3f44
alexlee-dev <alex@alexlee.dev>
2019-07-06 22:49:15 -0700

    Basic logging.

--- src/reporter/Reporter.ts
+++ src/reporter/Reporter.ts
@@ -1,7 +1,41 @@
-export class Reporter {
-  constructor(config, options) {}
+import figlet = require("figlet");
+import gradient = require("gradient-string");
+import boxen = require("boxen");
+import ProgressBar = require("progress");
+import chalk = require("chalk");
 
-  public onRunStart(results, options) {
-    console.log("run start");
+export class Reporter {
+  private completedTests: number;
+  private progressBar: ProgressBar;
+
+  constructor(config: any, options: any) {
+    this.completedTests = 0;
+    const borderStyle = boxen.BorderStyle.Double;
+    console.clear();
+    console.log(
+      boxen(
+        gradient.pastel(
+          figlet.textSync("react-scripts-reporter", {
+            font: "Slant"
+          })
+        ),
+        { borderStyle, float: "center", padding: 1 }
+      )
+    );
+  }
+
+  public onRunStart(results: any, options: any) {
+    this.progressBar = new ProgressBar(":percent", {
+      total: results.numTotalTestSuites
+    });
+  }
+
+  public onTestResult(testRunConfig: any, testResults: any, runResults: any) {
+    this.completedTests++;
+    this.progressBar.tick();
+  }
+
+  public onRunComplete(test: any, runResults: any) {
+    console.log(chalk.default.green("COMPLETE"));
   }
 }
