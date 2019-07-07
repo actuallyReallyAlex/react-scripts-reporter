51d3220682f1220df4e622368d5ffb99125953b8
alexlee-dev <alex@alexlee.dev>
2019-07-06 23:08:21 -0700

    Increase test coverage.

--- __tests__/fix.test.ts
+++
@@ -1,7 +0,0 @@
-import { entry } from "../src/entry";
-
-describe("Should work in build?", () => {
-  it("Should work.", () => {
-    const type = entry({});
-  });
-});
--- __tests__/subtract.example.ts
+++
@@ -1,7 +0,0 @@
-const subtract = (x, y) => x - y;
-
-describe("Test", () => {
-  it("Subtracts 4 from 5.", () => {
-    expect(subtract(5, 4)).toBe(1);
-  });
-});
