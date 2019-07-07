de08eac47bcf651cddab710ff00f237944bb4ac5
alexlee-dev <alex@alexlee.dev>
2019-07-07 09:04:59 -0700

    Very basic render out to an html file.

--- src/processor/Processor.ts
+++ src/processor/Processor.ts
@@ -5,9 +5,40 @@ export class Processor {
     return new Processor(results, config);
   }
 
-  constructor(results: any, config: any) {
-    console.log({ results, config });
-    // fs.writeFileSync("./report/index.html", );
-    console.log("File written");
+  constructor(results: jest.AggregatedResult, config: {}) {
+    const {
+      numFailedTestSuites,
+      numFailedTests,
+      numPassedTestSuites,
+      numPassedTests,
+      numPendingTestSuites,
+      numPendingTests
+    } = results;
+
+    if (!fs.existsSync("./report")) {
+      fs.mkdirSync("./report");
+    }
+
+    fs.writeFileSync(
+      "./report/report.json",
+      JSON.stringify({ ...results }, null, 2)
+    );
+
+    fs.writeFileSync(
+      "./report/index.html",
+      `<html>
+        <body>
+          <div>
+            <p>Number of Failed Test Suites: ${numFailedTestSuites}</p>
+            <p>Number of Failed Tests: ${numFailedTests}</p>
+            <p>Number of Passed Test Suites: ${numPassedTestSuites}</p>
+            <p>Number of Passed Tests: ${numPassedTests}</p>
+            <p>Number of Skipped Test Suites: ${numPendingTestSuites}</p>
+            <p>Number of Skipped Tests: ${numPendingTests}</p>
+          </div>
+        </body>
+      </html>`
+    );
+    console.log("Files written");
   }
 }
