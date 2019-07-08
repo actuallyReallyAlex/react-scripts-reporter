49a607f99dbf2d499a33c7ae6b63f19bd9b7e41a
alexlee-dev <alex@alexlee.dev>
2019-07-07 23:15:20 -0700

    ⚛️ Start to setup React.

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+// const path = require("path");
+
+module.exports = {
+  entry: "./src/renderer/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
+  // resolve: {
+  //   alias: {
+  //     Components: path.resolve(__dirname, "src/renderer/components/")
+  //   }
+  // }
+};
