07859c8bfd472f71e22073a6ca8bebf808899e9e
alexlee-dev <alex@alexlee.dev>
2019-07-14 11:52:13 -0700

    📦  Add Grommet

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { Grommet, grommet } from "grommet";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Grommet full theme={grommet}>
+    <App />
+  </Grommet>,
+  document.getElementById("root")
+);
