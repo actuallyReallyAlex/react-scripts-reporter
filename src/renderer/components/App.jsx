c0ad62bdad347ed3c2f725b5213863df7c728606
alexlee-dev <alex@alexlee.dev>
2019-07-14 17:19:12 -0700

    ✨ Ability to view test file(s)

--- src/renderer/components/App.jsx
+++ src/renderer/components/App.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Box, Text, Heading } from "grommet";
+import { Accordion, Box, Heading, Text, Paragraph } from "grommet";
+import TestResult from "./TestResult";
 
 const App = () => {
   useEffect(() => {
     axios.get("http://localhost:5000/report").then(response => {
-      console.log({ response });
+      console.log({ data: response.data });
       setReport(response.data);
     });
   }, []);
@@ -16,18 +17,31 @@ const App = () => {
     numPassedTestSuites: null,
     numPassedTests: null,
     numPendingTestSuites: null,
-    numPendingTests: null
+    numPendingTests: null,
+    testResults: []
   });
 
   return (
-    <Box align="center" fill justify="center">
+    <Box fill>
       <Heading level="1">Test Results</Heading>
-      <Text>Number of Failed Test Suites: {report.numFailedTestSuites}</Text>
-      <Text>Number of Failed Tests: {report.numFailedTests}</Text>
-      <Text>Number of Passed Test Suites: {report.numPassedTestSuites}</Text>
-      <Text>Number of Passed Tests: {report.numPassedTests}</Text>
-      <Text>Number of Skipped Test Suites: {report.numPendingTestSuites}</Text>
-      <Text>Number of Skipped Tests: {report.numPendingTests}</Text>
+      <Box fill="horizontal">
+        <Text>Number of Failed Test Suites: {report.numFailedTestSuites}</Text>
+        <Text>Number of Failed Tests: {report.numFailedTests}</Text>
+        <Text>Number of Passed Test Suites: {report.numPassedTestSuites}</Text>
+        <Text>Number of Passed Tests: {report.numPassedTests}</Text>
+        <Text>
+          Number of Skipped Test Suites: {report.numPendingTestSuites}
+        </Text>
+        <Text>Number of Skipped Tests: {report.numPendingTests}</Text>
+      </Box>
+      <Box align="center" fill="horizontal" justify="center">
+        <Accordion>
+          {report.testResults.map((testResult, i) => {
+            console.log({ testResult, i });
+            return <TestResult index={i} testResult={testResult} />;
+          })}
+        </Accordion>
+      </Box>
     </Box>
   );
 };
