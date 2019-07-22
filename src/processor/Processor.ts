5f44cf41bb475c11e333d82663143c6bc6690742
alexlee-dev <alex@alexlee.dev>
2019-07-21 18:02:12 -0700

    🎨 Add Favicon.

--- src/processor/Processor.ts
+++ src/processor/Processor.ts
@@ -14,6 +14,7 @@ export class Processor {
     this.createReportJSON(results);
     this.createReportHTML();
     this.createReportJS();
+    this.createAssets();
 
     console.log("Report generated.");
   }
@@ -35,5 +36,29 @@ export class Processor {
   public createReportJS() {
     const js = fs.readFileSync(path.resolve(__dirname, "../renderer/main.js"));
     fs.writeFileSync("./report/main.js", js);
+  }
+
+  public createAssets() {
+    const webmanifest = fs.readFileSync(
+      path.resolve(__dirname, "../renderer/assets/site.webmanifest")
+    );
+    const appleTouchIcon = fs.readFileSync(
+      path.resolve(__dirname, "../renderer/assets/apple-touch-icon.png")
+    );
+    const favicon32 = fs.readFileSync(
+      path.resolve(__dirname, "../renderer/assets/favicon-32x32.png")
+    );
+    const favicon16 = fs.readFileSync(
+      path.resolve(__dirname, "../renderer/assets/favicon-16x16.png")
+    );
+    const favicon = fs.readFileSync(
+      path.resolve(__dirname, "../renderer/assets/favicon.ico")
+    );
+
+    fs.writeFileSync("./report/site.webmanifest", webmanifest);
+    fs.writeFileSync("./report/apple-touch-icon.png", appleTouchIcon);
+    fs.writeFileSync("./report/favicon-32x32.png", favicon32);
+    fs.writeFileSync("./report/favicon-16x16.png", favicon16);
+    fs.writeFileSync("./report/favicon.ico", favicon);
   }
 }
